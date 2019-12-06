812e41a0d6a6947b603f26e58a580209d8694180
Abderrahmane Faiz <abderrahmane.faiz.50@gmail.com>
2019-12-06 11:12:57 +0100

    Interface Bootstrap

---
+++ interfaceSgbd/src/function.php
@@ -0,0 +1,28 @@
+<?php
+function display_data($data) {
+    $output = "<table>";
+    foreach($data as $key => $var) {
+        //$output .= '<tr>';
+        if($key===0) {
+            $output .= '<tr>';
+            foreach($var as $col => $val) {
+                $output .= "<td>" . $col . '</td>';
+            }
+            $output .= '</tr>';
+            foreach($var as $col => $val) {
+                $output .= '<td>' . $val . '</td>';
+            }
+            $output .= '</tr>';
+        }
+        else {
+            $output .= '<tr>';
+            foreach($var as $col => $val) {
+                $output .= '<td>' . $val . '</td>';
+            }
+            $output .= '</tr>';
+        }
+    }
+    $output .= '</table>';
+    echo $output;
+}
+?>
